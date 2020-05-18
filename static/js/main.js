eaece39f153c69ec63a25e4babbe367475e93ac4
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2020-05-18 09:54:01 +0000

    Fixed for new css

--- static/js/main.js
+++ static/js/main.js
@@ -210,7 +210,7 @@ function sortTable(table, n) {
 
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.nb-burger'), 0);
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
@@ -220,8 +220,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 const target = el.dataset.target;
                 const $target = document.getElementById(target);
                 // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
+                el.classList.toggle('on');
+                $target.classList.toggle('on');
             }
             );
         }
